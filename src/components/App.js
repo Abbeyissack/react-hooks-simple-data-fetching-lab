fbc3fe8a9562c38031bb6ba97df6422339133df7
Uwes Ahabab <uwesahabab@gmail.com>
2024-05-03 20:08:32 +0300

    Data Fetching

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,22 @@
 // create your App component here
+import React, { useEffect, useState } from "react";
+const App = () => {
+  const [image, setImage] = useState();
+  const [loaded, setIsloaded] = useState(false);
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((r) => r.json())
+      .then((data) => {
+        setImage(data.message);
+        setIsloaded(true);
+      });
+  }, []);
+  console.log(image);
+  if (!loaded) {
+    return <p>Loading...</p>;
+  }
+  return <img src={image} alt="A Random Dog" />;
+};
+
+// export your App component
+export default App;
